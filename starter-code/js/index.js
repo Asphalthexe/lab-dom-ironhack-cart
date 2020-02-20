f707c7caaefdc35b3b7bfcc8a0f2e1e89f95f3c5
Sonja Lesch <sonjalesch@gmx.de>
2020-02-20 19:08:34 +0100

    BER-WDPT202002-Sonja L. & Sjard

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,11 +1,43 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+
+
+
 function updateSubtot($product) {
   // Iteration 1.1
+  //let price = document.getElementsByClassName('pu')[0].innerText
+  let price = $product.querySelector('.pu span').innerText
+  //let quantity = document.getElementsByClassName('qty')[0].value
+  let quantity = $product.querySelector('.qty').value
+  total = price * quantity
 }
 
 function calcAll() {
   // Iteration 1.2
+  let subtotal = document.body.getElementsByClassName('subtot')[0]
+  subtotal.innerText = updateSubtot()
 }
 $calc.onclick = calcAll;
+
+
+
+
+
+/////////////////////////////////////////
+/*
+
+//tafel
+let qty = $product.querySelector('input').value
+
+//multiply and write into sub total
+$product.querySelector('.subtot span').innerText
+
+//funtion mit for loop?
+function calcAll() {
+  let firstProduct = 
+    document.getElementsByClassName('product')
+    updateSubtot(firstProduct)
+}
+
+*/
